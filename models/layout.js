919bc4d46e56146cc4b20101d60b726797d765d1
eman <raymande.leano@98labs.com>
2015-04-08 18:43:12 +0800

    do not use the manual ejs process for layout

--- models/layout.js
+++ models/layout.js
@@ -1,10 +1,19 @@
 module.exports = function(app){
 	app.use(function(req, res, next) {
 	    res.renderLayout = function(viewPage, options, layout){
+	    	var that = this;
 	        if(!layout) layout = 'main';
-	        var ejs = require('ejs');
+
+	        var appRootDir = __dirname + '/../'; // Change this depending on the location of this
+	        app.render(appRootDir + 'views/'+ viewPage, options, function(err, html){
+		        options.content = html;
+		        options.viewPage = viewPage;
+		        that.render('layouts/'+ layout, options);
+
+	        });
+
+	        /*var ejs = require('ejs');
 	        var fs = require('fs');
-	        options.site = app.locals.site;
 	        var appRootDir = __dirname + '/../';
 	        var content = ejs.compile(
 	                fs.readFileSync(appRootDir + 'views/'+ viewPage +'.ejs', 'utf8')
@@ -13,7 +22,7 @@ module.exports = function(app){
 	        // this.send(content);
 	        options.content = content;
 	        options.viewPage = viewPage;
-	        this.render('layouts/'+ layout, options);
+	        this.render('layouts/'+ layout, options);*/
 	    }
 	    next();
 	});
